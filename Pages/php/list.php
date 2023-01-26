be246f811171887ed730743ac57745f25b234db0
Alix <dev.alix13@gmail.com>
2023-01-26 23:38:45 +0330

    as always improve something :+1:

--- Pages/php/list.php
+++ Pages/php/list.php
@@ -1,22 +1,16 @@
-<?php
-$res = $Request->getTokenList();
-function secure($val)
-{
-    return (is_array($val)) ? array_map('secure', $val) : htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
-}
-
-$response = secure($res);
-?>
+<?php $response = secure($Request->getTokenList()); ?>
 <html>
+
 <body>
     <table class="table table-bordered table-responsive">
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">Game</th>
+                <th scope="col">appId</th>
                 <th scope="col">Memo</th>
                 <th scope="col">IS_expired</th>
                 <th scope="col">Token</th>
+                <th scope="col">Actions</th>
             </tr>
         </thead>
         <?php foreach ($response as $key => $item) {?>
@@ -24,26 +18,24 @@ $response = secure($res);
             <tr>
                 <th scope="row"><?=say($key)?></th>
                 <td class="font-monospace ">
-                    <!-- <abbr title="" > -->
+                    <abbr title="<?= appIdtoName($item['appid']) ?>">
                         <?=say($item['appid'])?>
-                    <!-- </abbr> -->
+                    </abbr>
                 </td>
                 <td class="font-monospace"><?=say($item['memo'])?></td>
-                <td
-                    class="font-monospace <?=say(($item['is_expired'] == 1 ? "text-bg-danger" : "text-bg-success"))?> ">
-                    <?=say(($item['is_expired'] == 0 ? 'No' : 'Yes'))?></td>
+                <td class="font-monospace <?=say(($item['is_expired'] == 1 ? "text-bg-danger" : "text-bg-success"))?> ">
+                    <?=say(($item['is_expired'] == 1 ? 'Yes' : 'No' ))?></td>
                 <td class="font-monospace" id="<?=say($key)?>"><?=say($item['login_token'])?></td>
                 <td class="col-2">
-                    <button onclick="CopyToClipboard(<?=say($key)?>)" class="p-m-1 btn btn-outline-dark"
-                        type="submit">
+                    <button onclick="CopyToClipboard(<?=say($key)?>)" class="p-m-1 btn btn-outline-dark" type="submit">
                         Copy Token
                     </button>
-                    <a href="?del&steamid=<?=say($item['steamid'])?>"><button
-                            class="p-m-1 btn btn-warning link-danger" type="submit">
+                    <a href="?del&steamid=<?=say($item['steamid'])?>"><button class="p-m-1 btn btn-warning link-danger"
+                            type="submit">
                             Delete
                         </button></a>
-                    <a href="?regen&steamid=<?=say($item['steamid'])?>"><button
-                            class="p-m-1 btn btn-dark link-success" type="submit">
+                    <a href="?regen&steamid=<?=say($item['steamid'])?>"><button class="p-m-1 btn btn-dark link-success"
+                            type="submit">
                             Regenerate
                         </button></a>
                 </td>
@@ -52,4 +44,5 @@ $response = secure($res);
         <?php }?>
     </table>
 </body>
+
 </html>
--- inc/Functions.php
+++ inc/Functions.php
@@ -1,8 +1,19 @@
 <?php
+function secure($val)
+{
+    return (is_array($val)) ? array_map('secure', $val) : htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
+}
+
 function appIdtoName(int $id)
 {
-    $json = file_get_contents(DATA . 'linux_compact.json');
+    $json = file_get_contents(DATA . 'appids.json');
     $data = json_decode($json, true);
-    var_dump($data);
-
+    for ($i = 0; $i < count($data); $i++) {
+        if ($data[$i]['APPiD'] == $id) {
+            return $data[$i]['SERVER_NAME'];
+            break;
+        } else {
+            return 'App id not found!!';
+        }
+    }
 }
